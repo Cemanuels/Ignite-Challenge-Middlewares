7235ffbcdfdcdf3095b3458c3b3f6dc51d0fd668
caioemanuel <caioemanuel@noar.tv>
2021-09-14 13:19:04 -0300

    finished challenge

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not exists!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ((user.pro === false && user.todos < 10) || user.pro === true) {
+    return next();
+  }
+
+  if (user.pro === false && user.todos === 10) {
+    return response.status(403).json({ error: "maximum todos limit to plan reached" })
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const userIndex = users.findIndex(user => user.username === username);
+
+  if (userIndex === -1) {
+    return response.status(404).json({ error: "user not exists!" })
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "invalid uuid!" })
+  }
+
+  const todo = users[userIndex].todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "todo not exists!" })
+  }
+
+  request.user = users[userIndex];
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not exists!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
